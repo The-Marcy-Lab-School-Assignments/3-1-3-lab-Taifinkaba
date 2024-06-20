b39a064da53a74bdcf903b4b808fb26ba80c7cde
Taifinkaba <taifinkabad@gmail.com>
2024-06-20 18:00:39 -0400

    changed stuff

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,42 +1,3 @@
-// export const getFirstThreeFantasyBooks = () => {
-//     const url = 'https://openlibrary.org/subjects/fantasy.json';
-
-//     return fetch(url)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Failed to get fantasy books');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         const firstThreeBooks = data.works.slice(0, 3).map(work => {
-//           const title = work.title;
-  
-//           let author = {};
-//           if (work.authors && work.authors.length > 0) {
-//             author.name = work.authors[0].name;
-//             author.urlKey = work.authors[0].key;
-//           }
-  
-//           const coverId = work.cover_id;
-//           const coverUrl = `https://covers.openlibrary.org/a/id/${coverId}-M.jpg`;
-  
-//           return {
-//             title,
-//             author,
-//             coverUrl
-//           };
-//         });
-  
-//         return firstThreeBooks;
-//       })
-//       .catch(error => {
-//         // errors and unsuccessful requests
-//         console.warn("Failed to get fantasy books", error.message);
-//         return null;
-//       });
-// };
-
 export const getFirstThreeFantasyBooks = async () => {
     const url = 'https://openlibrary.org/subjects/fantasy.json';
 
@@ -64,35 +25,6 @@ export const getFirstThreeFantasyBooks = async () => {
     }
 };
 
-// export const getAuthor = (urlKey) => {
-//     const baseUrl = 'https://openlibrary.org';
-//     const endpoint = `${baseUrl}${urlKey}.json`;
-  
-//     return fetch(endpoint)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Failed to get author');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         const formattedAuthor = {
-//           birthDate: data.birth_date,
-//           bio: data.bio,
-//           wikipediaLink: data.wikipedia,
-//           name: data.name,
-//           pictureUrl: data.photos && data.photos.length > 0
-//             ? `https://covers.openlibrary.org/b/id/${data.photos[0]}-M.jpg`
-//             : null
-//         };
-//         return formattedAuthor;
-//       })
-//       .catch(error => {
-//         console.warn(error.message);
-//         return null;
-//       });
-// };
-
 export const getAuthor = async (urlKey) => {
     const baseUrl = 'https://openlibrary.org';
     const endpoint = `${baseUrl}${urlKey}.json`;
@@ -106,7 +38,6 @@ export const getAuthor = async (urlKey) => {
 
         const data = await response.json();
 
-        // Format the author object as required
         const formattedAuthor = {
             birthDate: data.birth_date,
             bio: data.bio,
